f7e3206525d3489051d6ecb42ebd46b49fda3212
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-11-15 18:04:36 +0530

    CreatePost outline

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo:''
+  });
+
+  const [generaing, setGenerating] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Create imaginative and visually stunning images through AI and share them with the community.
+        </p>
+      </div>
+
+    </section>
   )
 }
 
-export default CreatePost
+export default CreatePost;
